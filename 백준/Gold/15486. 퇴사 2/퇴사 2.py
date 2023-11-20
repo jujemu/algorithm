5acc16698377cda5674510ee1bd51729e53191a9
zaimu <103489171+jujemu@users.noreply.github.com>
2023-11-20 19:31:51 +0900

    [Gold V] Title: 퇴사 2, Time: 1040 ms, Memory: 286652 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -2,20 +2,16 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 N = int(input())
-schedule = [[0]] + [list(map(int, input().split())) for _ in range(N)]
+schedule = [list(map(int, input().split())) for _ in range(N)]
 
 dp = [0] * (N+1)
 max_value = 0
 
-for idx in range(N, 0, -1):
+for idx in range(N-1, -1, -1):
     t, p = schedule[idx]
     if idx + t > N:
-        if idx+t-1 == N:
-            dp[idx] = max(max_value, p)
-            max_value = dp[idx]
-        else:
-            dp[idx] = max_value
+        dp[idx] = max_value
     else:
         dp[idx] = max(max_value, p + dp[idx+t])
         max_value = dp[idx]
-print(dp[1])
+print(dp[0])
