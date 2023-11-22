54297a6dfc5126959ef220256439a97c3c73fa7a
jujemu <jujemu30@gmail.com>
2023-11-22 20:25:47 +0900

    11725

--- test.py
+++ test.py
@@ -1,33 +1,21 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(10**6+1)
 
-N, M = map(int, input().split())
-NUMBER_OF_CHAIR = 20
+N = int(input())
+adj_matrix = [[] for _ in range(N+1)]
+for i in range(N-1):
+    node_1, node_2 = list(map(int, input().split()))
+    adj_matrix[node_1].append(node_2)
+    adj_matrix[node_2].append(node_1)
 
-trains = [[False]* (NUMBER_OF_CHAIR + 1) for _ in range(N)]
-for _ in range(M):
-    c = list(map(int, input().split()))
-    c[1] -= 1
-    if c[0] == 1:
-        trains[c[1]][c[2]] = True
-    elif c[0] == 2:
-        trains[c[1]][c[2]] = False
-    elif c[0] == 3:
-        for idx in range(NUMBER_OF_CHAIR-1, 0, -1):
-            trains[c[1]][idx+1] = trains[c[1]][idx]
-        trains[c[1]][1] = False
-    else:
-        for idx in range(2, NUMBER_OF_CHAIR+1):
-            trains[c[1]][idx-1] = trains[c[1]][idx]
-        trains[c[1]][-1] = False
-            
-s = set()
-result = 0
-for train in trains:
-    value2binary = 0
-    for i in range(1, NUMBER_OF_CHAIR+1):
-        value2binary += train[i] * 2**(i-1)
-    if value2binary not in s:
-        s.add(value2binary)
-        result += 1
-print(result)
+parent = [None] * (N+1)
+visited = [False] * (N+1)
+def dfs(cur_parent):
+    visited[cur_parent] = True
+    for adj in adj_matrix[cur_parent]:
+        if not visited[adj]:
+            parent[adj] = cur_parent
+            dfs(adj)
+dfs(1)
+print(*parent[2:], sep="\n")
