e4cde1aa1f91c08e5d4cd9d06cb97d6fc15b16aa
zaimu <103489171+jujemu@users.noreply.github.com>
2023-11-23 04:41:23 +0900

    [Gold V] Title: 동전 1, Time: 164 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -2,12 +2,13 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 n, k = map(int, input().split())
-coins = [int(input()) for _ in range(n)]
-coins = [coin for coin in coins if coin <= 10000]
+coins = {int(input()) for _ in range(n)}
+coins = [coin for coin in coins if coin <= k]
 
 dp = [0] * (k+1)
-dp[0] = 1
 for coin in coins:
-    for i in range(coin, k+1):
-        dp[i] += dp[i-coin]
-print(dp[-1])
+    dp[coin] += 1
+    for idx in range(1, k+1-coin):
+        if dp[idx]:
+            dp[idx+coin] += dp[idx]
+print(dp[k])
