ffea61ac0992b985dfb36d02b3268b74d1d981a6
zaimu <103489171+jujemu@users.noreply.github.com>
2023-11-23 15:55:34 +0900

    [Silver V] Title: 배열 합치기, Time: 1576 ms, Memory: 187240 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.py
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -1,24 +1,4 @@
-import sys
-input = lambda: sys.stdin.readline().rstrip()
-
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
-result = []
-
-a_idx, b_idx = 0, 0
-while True:
-    if a_idx >= N:
-        result.extend(B[b_idx:])
-        break
-    if b_idx >= M:
-        result.extend(A[a_idx:])
-        break
-
-    if A[a_idx] >= B[b_idx]:
-        result.append(B[b_idx])
-        b_idx += 1
-        continue
-    result.append(A[a_idx])
-    a_idx += 1
-print(*result, sep=" ")
+print(*sorted(A+B), sep=" ")
