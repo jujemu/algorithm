68bb46f3f8e5767615e5797bc73f26104699d15d
Jemu Ju <103489171+jujemu@users.noreply.github.com>
2023-12-28 00:31:27 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.08 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,26 +1,135 @@
-from collections import deque
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+import collections
+
+DUMMY_TRUCK = 0
+
+
+class Bridge(object):
+
+    def __init__(self, length, weight):
+        self._max_length = length
+        self._max_weight = weight
+        self._queue = collections.deque()
+        self._current_weight = 0
+
+    def push(self, truck):
+        next_weight = self._current_weight + truck
+        if next_weight <= self._max_weight and len(self._queue) < self._max_length:
+            self._queue.append(truck)
+            self._current_weight = next_weight
+            return True
+        else:
+            return False
+
+    def pop(self):
+        item = self._queue.popleft()
+        self._current_weight -= item
+        return item
+
+    def __len__(self):
+        return len(self._queue)
+
+    def __repr__(self):
+        return 'Bridge({}/{} : [{}])'.format(self._current_weight, self._max_weight, list(self._queue))
 
 
 def solution(bridge_length, weight, truck_weights):
-    idx, time = 0, 0
-    q, q_t, s = deque([]), deque([]), 0
+    bridge = Bridge(bridge_length, weight)
+    trucks = collections.deque(w for w in truck_weights)
 
-    while True:
-        time += 1
-        for i in range(len(q_t)):
-            q_t[i] += 1
+    for _ in range(bridge_length):
+        bridge.push(DUMMY_TRUCK)
 
-        if q_t and q_t[0] > bridge_length:
-            s -= q.popleft()
-            q_t.popleft()
+    count = 0
+    while trucks:
+        bridge.pop()
 
-        if len(q) <= bridge_length and idx < len(truck_weights) and s + truck_weights[idx] <= weight:
-            q.append(truck_weights[idx])
-            q_t.append(1)
-            s += truck_weights[idx]
-            idx += 1
+        if bridge.push(trucks[0]):
+            trucks.popleft()
+        else:
+            bridge.push(DUMMY_TRUCK)
 
-        if not q:
-            break
+        count += 1
 
-    return time
+    while bridge:
+        bridge.pop()
+        count += 1
+
+    return count
+
+
+def main():
+    print(solution(2, 10, [7, 4, 5, 6]), 8)
+    print(solution(100, 100, [10]), 101)
+    print(solution(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]), 110)
+
+
+if __name__ == '__main__':
+    main()
