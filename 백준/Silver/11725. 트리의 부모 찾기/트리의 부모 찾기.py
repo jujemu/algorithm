1e2cbde8c2b96979866ade45642d36a49c42fb8c
Jemu Ju <103489171+jujemu@users.noreply.github.com>
2024-01-19 11:02:53 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 324 ms, Memory: 65844 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -0,0 +1,28 @@
+import sys
+sys.setrecursionlimit(100001)
+# sys.stdin = open('./search/input_bj_11725.txt')
+input = sys.stdin.readline
+
+N = int(input())
+visit = [False] * (N+1)
+g = [[] for _ in range(N+1)]
+for _ in range(N-1):
+    x, y = map(int, input().split())
+    g[x].append(y)
+    g[y].append(x)
+
+result = [0]*(N+1)
+
+# dfs
+def dfs(vertex):
+    for adj in g[vertex]:
+        if not visit[adj]:
+            visit[adj] = True
+            result[adj] = vertex
+            dfs(adj)
+
+# traverse
+dfs(1)
+
+# output
+print(*result[2:], sep='\n')
