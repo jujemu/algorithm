99c3baaca5d7fdaecb87bf44bcf87c18380be6e0
Jemu Ju <103489171+jujemu@users.noreply.github.com>
2024-01-31 03:15:45 +0900

    [Gold V] Title: 퇴사 2, Time: 2100 ms, Memory: 251192 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,17 +1,28 @@
+'''
+https://www.acmicpc.net/problem/15486
+
+문제 풀이 과정
+
+'''
 import sys
+
+
 input = lambda: sys.stdin.readline().rstrip()
-
 N = int(input())
-schedule = [list(map(int, input().split())) for _ in range(N)]
+tasks = [tuple(map(int, input().split())) for _ in range(N)]
 
-dp = [0] * (N+1)
 max_value = 0
+dp = [0]*(N+2)
+for cur_d in range(N, 0, -1):
+    t, p = tasks[cur_d-1]
 
-for idx in range(N-1, -1, -1):
-    t, p = schedule[idx]
-    if idx + t > N:
-        dp[idx] = max_value
+    n_d = cur_d + t
+    if n_d <= N+1:
+        if max_value < dp[n_d]+p:
+            dp[cur_d] = dp[n_d]+p
+            max_value = dp[cur_d]
+        else:
+            dp[cur_d] = max_value
     else:
-        dp[idx] = max(max_value, p + dp[idx+t])
-        max_value = dp[idx]
-print(dp[0])
+        dp[cur_d] = max_value
+print(max_value)
