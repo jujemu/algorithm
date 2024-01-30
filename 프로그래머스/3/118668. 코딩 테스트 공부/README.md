90f630df1defae8dcc04e2f31fe2d9d9ed3dd8d5
Jemu Ju <103489171+jujemu@users.noreply.github.com>
2024-01-30 17:11:50 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 4.46 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/README.md
+++ 프로그래머스/3/118668. 코딩 테스트 공부/README.md
@@ -1,10 +1,10 @@
 # [level 3] 코딩 테스트 공부 - 118668 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118668%EF%BB%BF#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118668) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 7.30 ms
+메모리: 10.4 MB, 시간: 4.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 4:5:56
+2024년 1월 2일 17:11:48
 
 ### 문제 설명
 
--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
@@ -1,23 +1,28 @@
 def solution(alp, cop, problems):
-    max_alp = max(max(problems, key=lambda x: x[0])[0], alp)
-    max_cop = max(max(problems, key=lambda x: x[1])[1], cop)
-    
-    INF = int(1e9)
-    dp = [[INF] * (max_cop+1) for _ in range(max_alp+1)]
-    for r in range(alp+1):
-        for c in range(cop+1):
-            dp[r][c] = 0
-    
-    for r in range(alp, max_alp+1):
-        for c in range(cop, max_cop+1):
-            nr, nc = min(r+1, max_alp), min(c+1, max_cop)
-            dp[nr][c] = min(dp[nr][c], dp[r][c]+1)
-            dp[r][nc] = min(dp[r][nc], dp[r][c]+1)
-            
+    # 목표로 하는 alp와 cop를 구한다.
+    target_alp = max(max(problems, key=lambda x: x[0])[0], alp)
+    target_cop = max(max(problems, key=lambda x: x[1])[1], cop)
+    # dp table을 설정한다.
+    # 점화식은 문제를 풀거나 공부해서 얻은 alp, cop에서 최소 시간값을 구할 수 있도록 구성한다.
+    # 주의해야할 점은 문제를 풀 수 있으면 되기 때문에 목표로 하는 알고력과 코딩력을 넘어도 되게끔 해야한다.
+    INF = 1000
+    dp = [[INF] * (target_cop+1) for _ in range(target_alp+1)]
+    dp[alp][cop] = 0
+    for cur_alp in range(alp, target_alp+1):
+        for cur_cop in range(cop, target_cop+1):
+            cur = dp[cur_alp][cur_cop]
+
+            # 알고력과 코딩력을 학습할 때
+            if cur_cop+1 <= target_cop:
+                dp[cur_alp][cur_cop+1] = min(dp[cur_alp][cur_cop+1], cur+1)
+            if cur_alp+1 <= target_alp:
+                dp[cur_alp+1][cur_cop] = min(dp[cur_alp+1][cur_cop], cur+1)
+
+            # 문제를 풀 때
             for problem in problems:
                 alp_req, cop_req, alp_rwd, cop_rwd, cost = problem
-                if alp_req <= r and cop_req <= c:
-                    n_alp, n_cop = min(r+alp_rwd, max_alp), min(c+cop_rwd, max_cop)
-                    dp[n_alp][n_cop] = min(dp[n_alp][n_cop], dp[r][c] + cost)
-    print(*dp, sep="\n")
-    return dp[max_alp][max_cop]
+
+                if alp_req <= cur_alp and cop_req <= cur_cop:
+                    n_alp, n_cop = min(target_alp, cur_alp+alp_rwd), min(target_cop, cur_cop+cop_rwd)
+                    dp[n_alp][n_cop] = min(dp[n_alp][n_cop], cur+cost)
+    return dp[target_alp][target_cop]
