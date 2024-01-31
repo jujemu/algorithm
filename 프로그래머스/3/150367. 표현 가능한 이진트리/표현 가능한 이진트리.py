31931d44676be100f0ab4e1f9e212f7bb1eb91ce
Jemu Ju <103489171+jujemu@users.noreply.github.com>
2024-01-31 23:44:26 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 228.89 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
@@ -1,46 +1,34 @@
-def solution(numbers):
-    result = []
-    for number in numbers:
-        b_n = bin(number)[2:]
-        
-        length = len(b_n)
-        tree_length = find_tree_length(length)
-        b_n = "0"*(tree_length-length) + b_n
-        
-        left, right = 0, tree_length-1
-        root = b_n[(left+right)//2] != "1"
-        if dfs(b_n, left, right, root):
-            result.append(1)
-        else:
-            result.append(0)
-    return result
-        
-        
-def find_tree_length(length):
-    k = 1
-    while True:
-        k *= 2
-        if length <= k - 1:
-            return k-1
-        
-          
-def dfs(b_n, left, right, no_parent):
+def dnc(num, left, right):
     if left == right:
-        if no_parent:
-            if b_n[left] == "0":
-                return True
-            else:
-                return False
-        else:
-            return True
-    
-    mid = (left+right)//2
-    if no_parent:
-        if b_n[mid] == "1":
-            return False
-        return dfs(b_n, mid+1, right, True) and dfs(b_n, left, mid-1, True)
-    else:
-        if b_n[mid] == "0":
-            no_parent = True
-        return dfs(b_n, mid+1, right, no_parent) and dfs(b_n, left, mid-1, no_parent)
-    
+        return [True, int(num[left])]
+
+    mid = (left + right) // 2
+    root = int(num[mid])
+
+    left_subtree = dnc(num, left, mid - 1)
+    right_subtree = dnc(num, mid + 1, right)
+
+    flag = left_subtree[1] or right_subtree[1]
+
+    if flag == 1 and root == 0:
+        return [0, 0]
+
+    return [left_subtree[0] and right_subtree[0], root]
+
+def get_answer(num):
+    tmp = ''
+    while num > 0:
+        tmp = tmp + str(num % 2)
+        num //= 2
+    size = 1
+    while len(tmp) > pow(2, size) - 1:
+        size += 1
+    while len(tmp) < pow(2, size) - 1:
+        tmp += '0'
+
+    return int(dnc(tmp, 0, len(tmp) - 1)[0])
+
+
+def solution(numbers):
+    answer = [get_answer(num) for num in numbers]
+    return answer
