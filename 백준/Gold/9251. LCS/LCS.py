e7c9f13e7f5e76ea5b3c9792c1a257f397ca9f20
Jemu Ju <103489171+jujemu@users.noreply.github.com>
2024-02-01 15:36:26 +0900

    [Gold V] Title: LCS, Time: 156 ms, Memory: 118220 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,26 +1,19 @@
-import sys
-input = lambda: sys.stdin.readline().rstrip()
-
 A, B = input(), input()
-dp = [[0]*len(B) for _ in range(len(A))]
+A = " " + A
+B = " " + B
 
-for b_idx in range(len(B)):
-    if B[b_idx] == A[0]:
-        for idx in range(b_idx, len(B)):
-            dp[0][idx] = 1
-        break
+len_a, len_b = len(A), len(B)
+dp = [[0]*len_b for _ in range(len_a)]
+for r in range(len_a):
+    for c in range(len_b):
+        if r == 0 or c == 0:
+            continue
 
-for a_idx in range(len(A)):
-    if B[0] == A[a_idx]:
-        for idx in range(a_idx, len(A)):
-            dp[idx][0] = 1
-        break
-
-for a_idx in range(1, len(A)):
-    for b_idx in range(1, len(B)):
-        if A[a_idx] == B[b_idx]:
-            dp[a_idx][b_idx] = dp[a_idx-1][b_idx-1]+1
+        if A[r] == B[c]:
+            dp[r][c] = dp[r-1][c-1] + 1
         else:
-            dp[a_idx][b_idx] = max(dp[a_idx][b_idx-1], dp[a_idx-1][b_idx])
-# print(*dp, sep="\n")
-print(dp[-1][-1])
+            dp[r][c] = max(dp[r-1][c], dp[r][c-1])
+
+# find max
+result = max([max(row) for row in dp])
+print(result)
