b203535b27b81b58abeecc1a2b93549170b9c87f
Jemu Ju <103489171+jujemu@users.noreply.github.com>
2024-02-03 07:11:49 +0900

    [level 3] Title: 양과 늑대, Time: 0.57 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,7 +1,11 @@
+import sys
+sys.setrecursionlimit(10**6)
+
 def solution(info, edges):
-    global answer
+    global answer, cnt
     answer = 0
-    
+    cnt = 0
+
     grid = [[] for _ in range(len(info))]
     for a, b in edges:
         grid[a].append(b)
@@ -9,36 +13,38 @@ def solution(info, edges):
     init_next_list = [False] * len(info)
     for adj in grid[0]:
         init_next_list[adj] = True
-    
+
     dfs(0, init_next_list, [0,0], grid, info) 
-    
+
     return answer
 
 def dfs(cur_node, next_list, sheep_wolf, grid, info):
-    global answer
+    global answer, cnt
+    # cnt += 1
+    # if cnt > 10:
+    #     return
     next_list[cur_node] = False
-    
+
     if info[cur_node]:
         sheep_wolf[1] += 1
     else:
         sheep_wolf[0] += 1
-    
+
     if sheep_wolf[0] - sheep_wolf[1] <= 0:
         return
-    
+
     if answer < sheep_wolf[0]:
         answer = sheep_wolf[0]
-    
+
     for adj in grid[cur_node]:
         next_list[adj] = True
-    
+
     for idx in range(len(info)):
         if next_list[idx]:
             tmp = tuple(sheep_wolf)
             dfs(idx, next_list, sheep_wolf, grid, info)
             next_list[idx] = True
             sheep_wolf = list(tmp)
-    
+
     for adj in grid[cur_node]:
         next_list[adj] = False
-        
