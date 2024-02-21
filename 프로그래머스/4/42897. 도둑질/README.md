c9f6daf4c5085a7890fafe7338cdf4e3e59b2461
Jemu Ju <103489171+jujemu@users.noreply.github.com>
2024-02-21 13:31:04 +0900

    [level 4] Title: 도둑질, Time: 522.32 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/README.md
+++ 프로그래머스/4/42897. 도둑질/README.md
@@ -1,10 +1,10 @@
 # [level 4] 도둑질 - 42897 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42897) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42897?language=python3) 
 
 ### 성능 요약
 
-메모리: 88.5 MB, 시간: 576.63 ms
+메모리: 88.6 MB, 시간: 522.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 2일 17:15:53
+2024년 2월 3일 13:31:1
 
 ### 문제 설명
 
--- 프로그래머스/4/42897. 도둑질/도둑질.py
+++ 프로그래머스/4/42897. 도둑질/도둑질.py
@@ -1,22 +1,19 @@
 def solution(money):
     dp_1 = money[:-1]
     result_1 = dp(dp_1)
-    
+
     dp_2 = money[1:]
     result_2 = dp(dp_2)
-    
+
     return max(result_1, result_2)
 
-    
+
 def dp(arr):
     n = len(arr)
-    if n == 0:
-        return 0
-    
     if n == 1:
         return arr[0]
-    
+
     arr[1] = max(arr[:2])
     for i in range(2, n):
-        arr[i] = max(arr[i-2]+arr[i], arr[i-1])
+        arr[i] = max(arr[i - 2] + arr[i], arr[i - 1])
     return arr[-1]
