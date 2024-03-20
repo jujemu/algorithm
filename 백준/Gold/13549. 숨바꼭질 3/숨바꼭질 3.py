c2df1e869ff76cb142c8133916d6b0f809343457
Jemu Ju <jujemu30@gmail.com>
2024-03-20 12:27:29 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 3196 ms, Memory: 114432 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+
+N, K = map(int, input().split())
+
+n = 200_000
+answer = n
+visited = [False] * n
+q = deque([(N, 0)])
+while q:
+    p, time = q.popleft()
+
+    visited[p] = True
+    if p == K:
+        answer = min(answer, time)
+
+    # 2배 위치로
+    np = p*2
+    if np < n and not visited[np]:
+        q.append((np, time))
+
+    np_1, np_2 = p-1, p+1
+    if 0 <= np_1 < n and not visited[np_1]:
+        q.append((np_1, time+1))
+    if 0 <= np_2 < n and not visited[np_2]:
+        q.append((np_2, time+1))
+
+print(answer)
