c6c6528ba67a3bc09f9787cb6f59e6f51c73128f
Jemu Ju <jujemu30@gmail.com>
2024-03-22 17:02:07 +0900

    [Silver III] Title: 표절, Time: 116 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Silver/2428. 표절/표절.py
+++ 백준/Silver/2428. 표절/표절.py
@@ -2,6 +2,14 @@
 https://www.acmicpc.net/problem/2428
 
 주제: 투 포인터
+시간복잡도: N
+
+구현
+오름차순 정렬 후에 작은 원소부터 초점을 맞추고
+검사하지 않아도 되는 다른 원소 짝을 찾는다.
+짝을 찾으면 두 원소 사이의 값과 작은 원소는 쌍을 이룰 수 있으므로 count
+만약, 검사하지 않아도 되는 다른 원소 짝을 찾을 수 없을 때는
+다른 원소에 대해서 모두 쌍을 이룰 수 있음을 의미
 '''
 import sys
 input = lambda: sys.stdin.readline().rstrip()
@@ -12,8 +20,7 @@ def tp(sorted_arr):
     if length == 1:
         return 0
 
-    cnt = 0
-    p_2 = 1
+    cnt, p_2 = 0, 1
     for p_1 in range(length-1):
         if p_1 == p_2:
             p_2 = p_1+1
@@ -24,7 +31,7 @@ def tp(sorted_arr):
         if p_2 != length:
             cnt += p_2 - p_1 - 1
         else:
-            cnt += length - p_1 - 1
+            cnt += (length-1) - p_1
 
     return cnt
 
