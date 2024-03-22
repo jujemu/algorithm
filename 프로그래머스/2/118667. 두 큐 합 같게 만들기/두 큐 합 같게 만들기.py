a75d0f2b919507687d7d389f2f906aa9734c0126
Jemu Ju <jujemu30@gmail.com>
2024-03-22 20:21:24 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 98.79 ms, Memory: 22.8 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,25 +1,25 @@
-from collections import deque
-
 def solution(queue1, queue2):
-    tmp = queue1 + queue2 + queue1
-    q = (tmp)
-    p_1, p_2 = 0, len(queue1)
-
-    s = sum(queue1 + queue2)
-    if s % 2:
-        return -1
-    target = sum(queue1 + queue2) // 2
-
-    cnt, cur_s = 0, sum(queue1)
-    while cnt <= len(queue1)*3:
-        if cur_s == target:
-            return cnt
-
-        cnt += 1
-        if cur_s > target:
-            cur_s -= q[p_1]
-            p_1 += 1
-        else:
-            cur_s += q[p_2]
-            p_2 += 1
+    length = len(queue1)
+    window_length = length*2
+    pointer = length-1
+    s1, s2 = sum(queue1), sum(queue2)
+    
+    answer = 0
+    q = queue1 + queue2 + queue1 + queue2
+    for i in range(window_length):
+        while pointer < window_length+i and s1 < s2:
+            pointer += 1
+            answer += 1
+            
+            s1 += q[i+pointer]
+            s2 -= q[i+pointer]
+        
+        if s1 == s2:
+            return answer
+        
+        s2 += q[i+window_length]
+        s1 -= q[i]
+        pointer -= 1
+        answer += 1
+    
     return -1
