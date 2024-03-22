cf5588c0e328af2433f2464fecfc7b075e517ae6
Jemu Ju <jujemu30@gmail.com>
2024-03-22 21:03:24 +0900

    [Gold IV] Title: 회전 초밥, Time: 3192 ms, Memory: 164008 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.py
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.py
@@ -1,43 +1,37 @@
-'''
-https://www.acmicpc.net/problem/15961
-
-주제
-'''
-from collections import defaultdict
 import sys
+
+
 input = lambda: sys.stdin.readline().rstrip()
-
-
 N, d, k, c = map(int, input().split())
-sushis = [int(input()) for _ in range(N)]
-sushis += sushis[:k]
+foods = [int(input()) for _ in range(N)]
+window_size = len(foods)
+foods += foods
 
-# init calculate
-cur_sushi = defaultdict(int)
-for sushi in sushis[:k]:
-    cur_sushi[sushi] += 1
-check = True
-for sushi in cur_sushi:
-    if sushi == c:
-        check = False
-cur_count = len(cur_sushi)
-max_count = cur_count+1 if check else cur_count
+unique = 0
+eaten = [0] * (d+1)
+for i in range(k):
+    curr = foods[i]
+    if not eaten[curr]:
+        unique += 1
+    eaten[curr] += 1
+max_value = unique
+if not eaten[c]:
+    max_value += 1
 
-# calculate
-p_1 = 0
-for p_2 in range(k, N+k-1):
-    s_1, s_2 = sushis[p_1], sushis[p_2]
+for i in range(window_size):
+    left = foods[i]
+    eaten[left] -= 1
+    if not eaten[left]:
+        unique -= 1
 
-    cur_sushi[s_1] -= 1
-    if not cur_sushi[s_1]:
-        cur_count -= 1
+    right = foods[i + k]
+    if not eaten[right]:
+        unique += 1
+    eaten[right] += 1
 
-    if not cur_sushi[s_2]:
-        cur_count += 1
-    cur_sushi[s_2] += 1
+    if not eaten[c]:
+        max_value = max(unique+1, max_value)
+    else:
+        max_value = max(unique, max_value)
 
-    p_1 += 1
-    max_count = max(max_count, cur_count+1 if not cur_sushi[c] else cur_count)
-
-# print result
-print(max_count)
+print(max_value)
