61308ad89fac9472ac59eb211255fdd139b826d5
Jemu Ju <jujemu30@gmail.com>
2024-04-02 21:41:05 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 35.16 ms, Memory: 19.1 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,25 +1,23 @@
 def solution(queue1, queue2):
     length = len(queue1)
-    window_length = length*2
-    pointer = length-1
+    total_length = length * 2
     s1, s2 = sum(queue1), sum(queue2)
     
     answer = 0
-    q = queue1 + queue2 + queue1 + queue2
-    for i in range(window_length):
-        while pointer < window_length+i and s1 < s2:
-            pointer += 1
+    q = queue1 + queue2
+    p1 = 0
+    for p2 in range(length, total_length):
+        while s1 > s2:
+            s1 -= q[p1]
+            s2 += q[p1]
+            p1 += 1
             answer += 1
-            
-            s1 += q[i+pointer]
-            s2 -= q[i+pointer]
         
         if s1 == s2:
             return answer
         
-        s2 += q[i+window_length]
-        s1 -= q[i]
-        pointer -= 1
+        s1 += q[p2]
+        s2 -= q[p2]
         answer += 1
-    
-    return -1
+        
+    return answer if answer >= total_length else -1
