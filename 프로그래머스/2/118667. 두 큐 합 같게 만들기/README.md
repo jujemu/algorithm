03d1565d81ed32948e99d4cac04bf5380a0ecc9e
Jemu Ju <jujemu30@gmail.com>
2024-04-10 23:10:19 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 35.35 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 두 큐 합 같게 만들기 - 118667 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667?language=python3) 
 
 ### 성능 요약
 
-메모리: 19.1 MB, 시간: 35.16 ms
+메모리: 18.9 MB, 시간: 35.35 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 02일 21:41:03
+2024년 04월 10일 23:10:17
 
 ### 문제 설명
 
--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -2,7 +2,7 @@ def solution(queue1, queue2):
     length = len(queue1)
     total_length = length * 2
     s1, s2 = sum(queue1), sum(queue2)
-    
+
     answer = 0
     q = queue1 + queue2
     p1 = 0
@@ -12,12 +12,12 @@ def solution(queue1, queue2):
             s2 += q[p1]
             p1 += 1
             answer += 1
-        
+
         if s1 == s2:
             return answer
-        
+
         s1 += q[p2]
         s2 -= q[p2]
         answer += 1
-        
+
     return answer if answer >= total_length else -1
