8b9701288b999508f27751b34e1c1d060ae94e75
Jemu Ju <jujemu30@gmail.com>
2024-06-28 22:15:28 +0900

    [Silver IV] Title: 듣보잡, Time: 72 ms, Memory: 47952 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -2,18 +2,16 @@ import sys
 
 
 input = lambda: sys.stdin.readline().rstrip()
-N, M = map(int, input().split())
-듣도 = set(
-    input() for _ in range(N)
-)
-보도 = sorted([
-    input() for _ in range(M)
-])
+n, m = map(int, input().split())
+듣도 = [
+    input() for _ in range(n)
+]
+보도 = [
+    input() for _ in range(m)
+]
 
-answer = []
-i = 0
-for s in 보도:
-    if s in 듣도:
-        answer.append(s)
-print(len(answer))
-print(*answer, sep="\n")
+result = set(듣도) & set(보도)
+print(len(result))
+print(*sorted(
+    result
+), sep="\n")
